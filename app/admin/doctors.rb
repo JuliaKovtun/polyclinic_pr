cf47c8e79bff427e93878dc7ce39b566b98f629e
JuliaKovtun <julia141407@gmail.com>
2022-04-16 16:22:09 +0300

    Add show profile and doctors page

--- app/admin/doctors.rb
+++ app/admin/doctors.rb
@@ -1,16 +1,24 @@
 ActiveAdmin.register Doctor do
-  permit_params :email, :first_name, :last_name, :phone, :password, :password_confirmation
+  permit_params :email, :first_name, :last_name, :phone, :password, :password_confirmation, :category_id
   
   index do
     column :email
     column :first_name
     column :last_name
     column :phone
+    column :category
     column :remember_created_at
+    actions
   end
 
   form do |f|
-    f.inputs :email, :first_name, :last_name, :phone, :password, :password_confirmation
+    f.inputs :email, 
+             :first_name, 
+             :last_name, 
+             :phone, 
+             :password, 
+             :password_confirmation,
+             :category #, as: :select, collection: Category.select(:title).uniq
     f.button :Submit
   end
 end
--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -1,6 +1,18 @@
 module Users
     class UsersController < ApplicationController
+        before_action :authenticate_user!
+        before_action :user
+
         def index
+            @doctors = Doctor.all
+        end
+
+        def show 
+            
+        end
+
+        def user
+            @user = current_user
         end
     end
 end
